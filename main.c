b1cb48271fe18bda94df1d864790208a0a226014
satiwill <william.sati6@gmail.com>
2019-04-01 00:15:20 -0400

    added function that creates rings when you activate switch 1

--- main.c
+++ main.c
@@ -6,7 +6,8 @@
 #include "boardSetupFunctions.h"
 #include "manageUserInputFunctions.h"
 #include "boatFunctions.h"
-
+#include "stdbool.h" 
+#include "stdlib.h"
 
 
 //global variable definition (memory is allocated)
@@ -15,6 +16,11 @@ double dx_Boat=0;//default 0;
 double dy_Boat=0;
 
 //320x240
+void drawRing(int rippleRadius, int rippleCenter_x,int rippleCenter_y,short int ring_Color);
+void updateRingPosition(int* rippleCenter_x, int* rippleCenter_y, int* rippleRadius, volatile int* switchData, bool* drawingRipple, int* furthestPoint,int* furthestVisibleDistance, bool* lastRipple);
+void plotEightSymmetricPixels(int xCenter, int yCenter, int x, int y, short int ring_Color);
+void updateFurthestPoint(int rippleCenter_x, int rippleCenter_y, int* furthestPoint);
+bool screenContains(int x, int y);
 
 int main(void){
 	
@@ -30,6 +36,7 @@ enable_A9_interrupts(); // enable interrupts
 	int width = 8;
 	int height = 8;
 	short int color_Boat = 0xFD00;//orange
+	short int ring_Color = 0x00FF;//Blueish
 	int x_Boat=156;
 	int y_Boat=114;
 	
@@ -59,25 +66,35 @@ enable_A9_interrupts(); // enable interrupts
 	
 	
 	volatile int* keyData = (int *) 0xFF200050;
+	volatile int* switchData = (int *) SW_BASE;
 	
 	int black = 0;
 	int previous_x_Boat = x_Boat;
 	int previous_y_Boat = y_Boat;
-	
-	
+	int previousRippleRadius = 1;
+	int rippleRadius = 1;
+	int rippleCenter_x;
+	int rippleCenter_y;
+	bool drawingRipple = 0;
+	bool lastRipple = 0;
+	int furthestPoint[2];
+	int furthestVisibleDistance = 0;
     while (1)
     {
-		//update previous boat_position before changing the boat position
+		//update previous boat_position and ring_position before changing the boat position
 		previous_x_Boat = x_Boat;
 		previous_y_Boat = y_Boat;
+		previousRippleRadius = rippleRadius;
+		
 		
 		//make sure the updated position will be valid, then update the boat's position
 		updateBoatPositionAndSpeed(&x_Boat,&y_Boat,width,height,keyData);
-
+		updateRingPosition(&rippleCenter_x, &rippleCenter_y,&rippleRadius, switchData, &drawingRipple, furthestPoint, &furthestVisibleDistance, &lastRipple);
 		
 		//now that the new position is valid, draw the new box on the back_buffer
 		drawBox(x_Boat,y_Boat,width, height, color_Boat);
-		
+		if(drawingRipple)
+			drawRing(rippleRadius, rippleCenter_x,rippleCenter_y,ring_Color);
 
 		//now that everything is loaded properly, wait for the dma to switch front and back buffer addresses.
 		wait_for_vsync(pixelStatusPtr,front_buffer); // swaps front and back buffers on VGA vertical sync
@@ -85,13 +102,110 @@ enable_A9_interrupts(); // enable interrupts
 		
 		/* Erase any boxes and lines that were drawn in the last iteration */
 		drawBox(previous_x_Boat,previous_y_Boat,width, height, black);
+		if(drawingRipple || lastRipple){
+			drawRing(previousRippleRadius, rippleCenter_x, rippleCenter_y, black);
+			lastRipple=0;
+		}
     }
 }
 
+void updateRingPosition(int* rippleCenter_x, int* rippleCenter_y, int* rippleRadius, volatile int* switchData, bool* drawingRipple, int* furthestPoint,int* furthestVisibleDistance,bool* lastRipple){
+	if(!*drawingRipple){
+		if((*switchData & 1) == 0){ //check switch 1. If it's on, make a ripple, if it's not, don't make a ripple.
+			;
+		}else{
+			*drawingRipple = 1;
+			*rippleCenter_x = rand()%320;
+			*rippleCenter_y = rand()%240;
+			*rippleRadius = 0;
+			
+			*furthestVisibleDistance = 400;
+		}
+		return;		
+	}
+	
+	if(*rippleRadius > *furthestVisibleDistance){
+		*drawingRipple = 0;
+		*lastRipple = 1;
+	}else{
+		*rippleRadius+=1;
+	}
+	
+}
+void updateFurthestPoint(int rippleCenter_x, int rippleCenter_y, int* furthestPoint){
+	if(rippleCenter_x > 159.5 && rippleCenter_y > 119.5){
+		furthestPoint[0] = 0;
+		furthestPoint[1] = 0;
+	}
+	if(rippleCenter_x > 159.5 && rippleCenter_y < 119.5){
+		furthestPoint[0] = 0;
+		furthestPoint[1] = 239;
+	}
+	if(rippleCenter_x < 159.5 && rippleCenter_y > 119.5){
+		furthestPoint[0] = 319;
+		furthestPoint[1] = 0;
+	}
+	if(rippleCenter_x < 159.5 && rippleCenter_y < 119.5){
+		furthestPoint[0] = 319;
+		furthestPoint[1] = 239;
+	}
+}
 
+void drawRing(int rippleRadius, int rippleCenter_x,int rippleCenter_y,short int ring_Color){
+	//the following is Bresenham's algorithm.
+	
+	int x = 0, y = rippleRadius;
+	int d = 3 - 2 * rippleRadius;
+	plotEightSymmetricPixels(rippleCenter_x, rippleCenter_y, x, y, ring_Color);
+	while(y >= x)
+	{
+		x+=1;
+		if(d > 0)
+		{
+			y--;
+			d = d + 4 * (x - y) + 10;
+		}
+		else{
+			d = d + 4 * x + 6;
+		}
+	plotEightSymmetricPixels(rippleCenter_x,rippleCenter_y,x,y,ring_Color);
+	}
+}
 
+void plotEightSymmetricPixels(int xCenter, int yCenter, int x, int y, short int ring_Color){
+	
+	if(screenContains(xCenter+x,yCenter+y))
+		plot_pixel(xCenter+x,yCenter+y,ring_Color);
+	
+	if(screenContains(xCenter-x,yCenter+y))
+		plot_pixel(xCenter-x,yCenter+y,ring_Color);
+	
+	if(screenContains(xCenter+x,yCenter-y))
+		plot_pixel(xCenter+x,yCenter-y,ring_Color);
+	
+	if(screenContains(xCenter-x,yCenter-y))
+		plot_pixel(xCenter-x,yCenter-y,ring_Color);
+	
+	if(screenContains(xCenter+y,yCenter+x))
+		plot_pixel(xCenter+y,yCenter+x,ring_Color);
+	
+	if(screenContains(xCenter-y,yCenter+x))
+		plot_pixel(xCenter-y,yCenter+x,ring_Color);
+	
+	if(screenContains(xCenter+y,yCenter-x))
+		plot_pixel(xCenter+y,yCenter-x,ring_Color);
+	
+	if(screenContains(xCenter-y,yCenter-x))
+		plot_pixel(xCenter-y,yCenter-x,ring_Color);
+}
 
-
+bool screenContains(int x, int y){
+	if(x < 320 && x >= 0 && y < 240 && y >=0){
+		return true;
+	}else{
+		return false;
+	}
+}
 
 
 
