dcdf16e816abb23eb1e725b6ace804e56affaf54
satiwill <william.sati6@gmail.com>
2019-03-31 20:37:03 -0400

    created a new file to put all of the funtions that control the boat

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include "externalVariables.h"
 #include "boardSetupFunctions.h"
 #include "manageUserInputFunctions.h"
+#include "boatFunctions.h"
 
 
 
@@ -14,7 +15,6 @@ double dx_Boat=0;//default 0;
 double dy_Boat=0;
 
 //320x240
-void updateBoatPositionAndSpeed(int* x_Boat, int* y_Boat, int width, int height, volatile int* keyData);
 
 int main(void){
 	
@@ -91,50 +91,7 @@ enable_A9_interrupts(); // enable interrupts
 
 
 
-void updateBoatPositionAndSpeed(int* x_Boat, int* y_Boat, int width, int height, volatile int* keyData){
-	
-	//poll the user's input to updsate dx_Boat and dy_Boat
-		if(*keyData!=0){
-			manageKeyPress(keyData);
-		}
-	
-	//make sure the boat's position will be inbounds if we add dx_Boat and dy_Boat. If it's not, kill the boat's speed.
-if(*x_Boat + (int)dx_Boat > 320-(width-1)){
-			dx_Boat = 0;
-			*x_Boat = 320-(width-1);
-		}
-		else if(*x_Boat + (int)dx_Boat < 0){
-			dx_Boat = 0;
-			*x_Boat = 0;
-		}
-		else{
-			*x_Boat = *x_Boat + dx_Boat;
-		}
-		
-		if(*y_Boat + dy_Boat > 240-(height-1)){
-			dy_Boat = 0;
-			*y_Boat = 240-(height-1);
-		} else if(*y_Boat + dy_Boat < 0){
-			dy_Boat = 0;
-			*y_Boat = 0;
-		} else{
-			*y_Boat = *y_Boat + dy_Boat;
-		}
-		
-		//account for the drag force of the water which decelerates the boat.
-		if(dx_Boat>0){
-			dx_Boat -= pow(dx_Boat,2)/75+0.01;
-		}
-		if(dx_Boat<0){
-			dx_Boat += pow(dx_Boat,2)/75+0.01;
-		}
-		if(dy_Boat>0){
-			dy_Boat -= pow(dy_Boat,2)/75+0.01;
-		}
-		if(dy_Boat<0){
-			dy_Boat += pow(dy_Boat,2)/75+0.01;
-		}
-}
+
 
 
 
